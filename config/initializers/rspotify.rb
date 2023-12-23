4138f1c078f6b830394529006cb2e61290b79c62
Alvaro Delgado <aadelgado@fevaq.net>
2023-12-23 16:45:16 -0600

    change playlist id back

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -3,6 +3,6 @@
 client_id = '7f426fb55fbb418ba81f609600fd4f70'
 client_secret = 'f785f0d1d4734f23b251df05ba9bd972'
 
-SPOTIFY_PLAYLIST_ID = '2IdE94KF0DH5KWkrsVNB6v'
+SPOTIFY_PLAYLIST_ID = '4qzNPkgiGoNdYgfnumq733'
 
 RSpotify.authenticate(client_id, client_secret) if client_id.present? && client_secret.present?
